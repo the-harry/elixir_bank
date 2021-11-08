b01ae37a09fe5a588f1762dcd3d351fc05dc03c0
Harry <matheusthebr@gmail.com>
2021-11-07 21:48:42 -0300

    calc transactions report.

--- lib/transacao.ex
+++ lib/transacao.ex
@@ -5,7 +5,7 @@ defmodule Transacao do
 
   def gravar(tipo, de, valor, data, para \\ nil) do
     transacoes =
-      busca_todas() ++
+      todas() ++
         [%__MODULE__{tipo: tipo, de: de, valor: valor, data: data, para: para}]
 
     File.write(@transacoes, :erlang.term_to_binary(transacoes))
@@ -13,9 +13,28 @@ defmodule Transacao do
     transacoes
   end
 
-  defp busca_todas() do
+  def todas(), do: transacoes()
+
+  def por_ano(ano), do: Enum.filter(transacoes(), &(&1.data.year == ano))
+
+  def por_mes(ano, mes),
+    do: Enum.filter(transacoes(), &(&1.data.year == ano && &1.data.month == mes))
+
+  def por_dia(data), do: Enum.filter(transacoes(), &(&1.data == data))
+
+  defp transacoes() do
     {:ok, binario} = File.read(@transacoes)
 
     :erlang.binary_to_term(binario)
+  end
+
+  def calcular_mes(ano, mes), do: calcular(por_mes(ano, mes))
+
+  def calcular_ano(ano), do: calcular(por_ano(ano))
+
+  def calcular_dia(data), do: calcular(por_dia(data))
+
+  defp calcular(transacoes) do
+    {transacoes, Enum.reduce(transacoes, 0, fn x, acc -> acc + x.valor end)}
   end
 end
